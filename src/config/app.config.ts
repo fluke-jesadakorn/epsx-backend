3600f37917cb1b28f38b0753f4cd1ba486872f94
Fluke <fluke@Fluke16.local>
2025-02-03 14:20:37 +0700

    deploy

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -39,7 +39,7 @@ export interface AppConfig {
 }
 
 export const getAppConfig = (): AppConfig => {
-  const port = Number(process.env.PORT) || 3000;
+  const port = Number(process.env.PORT);
 
   return {
     port,
